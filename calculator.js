f7ae9941e8de44612257e277b362a46cfe4d78a2
Deborah Clerckx <80418872+Debbyckx@users.noreply.github.com>
2024-02-19 18:13:57 +0100

    adding files

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let buffer = "0";
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function displayMinOne(){
+    if(display.value.length === 1){
+        display.value = "";
+    } else{
+        display.value = display.value.substring(0, display.value.length - 1);
+    }
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
